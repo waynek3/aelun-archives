1dfdd8f951c5ea8b3ec73dcd0f991b83bb4355a4
waynek3 <wkraemer3@gmail.com>
2025-07-31 17:39:39 -0400

    fix: D-pad controls now respond correctly to touch events

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // This is the configuration for our game
 const config = {
-    type: Phaser.AUTO, // Automatically choose between WebGL or Canvas
-    width: 800,        // Game width in pixels
-    height: 600,       // Game height in pixels
-    physics: {         // We're enabling the built-in Arcade Physics engine
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
@@ -18,56 +18,91 @@ const config = {
 };
 
 // --- GLOBAL VARIABLES ---
-// It's often useful to have some variables accessible by all functions
 let player;
 let cursors;
-const playerSpeed = 200; // Pixels per second
+const playerSpeed = 200;
+
+const dpadState = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+};
 
 // Create a new Phaser Game instance
 const game = new Phaser.Game(config);
 
-// The preload function is where you load assets like images and sounds
 function preload() {
-    // Phaser has a built-in loader for images.
-    // We'll use a simple block from the official Phaser examples.
-    // The first argument is the 'key' we'll use to refer to this image.
     this.load.image('player', 'https://labs.phaser.io/assets/sprites/block.png');
 }
 
-// The create function is where you set up your game scene (sprites, text, etc.)
 function create() {
-    // Add the player sprite to the game.
-    // this.physics.add.sprite() creates a sprite with physics enabled.
-    // We're placing it in the center of the screen to start.
     player = this.physics.add.sprite(config.width / 2, config.height / 2, 'player');
-
-    // Make sure the player can't move outside the game world's boundaries.
     player.setCollideWorldBounds(true);
 
-    // Set up the keyboard controls.
-    // createCursorKeys() creates an object with properties for up, down, left, right.
     cursors = this.input.keyboard.createCursorKeys();
+
+    createDpad.call(this);
 }
 
-// The update function runs every frame (about 60 times per second)
 function update() {
-    // --- PLAYER MOVEMENT ---
-
-    // First, we reset the player's velocity to 0 each frame.
-    // This makes the movement feel snappy and stops the player when no key is pressed.
     player.setVelocity(0);
 
-    // Now, we check which keys are currently held down and set the velocity accordingly.
-    // This allows for diagonal movement.
-    if (cursors.left.isDown) {
+    const keyLeft = cursors.left.isDown;
+    const keyRight = cursors.right.isDown;
+    const keyUp = cursors.up.isDown;
+    const keyDown = cursors.down.isDown;
+
+    const dpadLeft = dpadState.left;
+    const dpadRight = dpadState.right;
+    const dpadUp = dpadState.up;
+    const dpadDown = dpadState.down;
+
+    if (keyLeft || dpadLeft) {
         player.setVelocityX(-playerSpeed);
-    } else if (cursors.right.isDown) {
+    } else if (keyRight || dpadRight) {
         player.setVelocityX(playerSpeed);
     }
 
-    if (cursors.up.isDown) {
+    if (keyUp || dpadUp) {
         player.setVelocityY(-playerSpeed);
-    } else if (cursors.down.isDown) {
+    } else if (keyDown || dpadDown) {
         player.setVelocityY(playerSpeed);
     }
+}
+
+function createDpad() {
+    const buttonSize = 50;
+    const alpha = 0.5;
+    const dpadX = 100;
+    const dpadY = this.cameras.main.height - 100;
+
+    const upButton = this.add.rectangle(dpadX, dpadY - buttonSize / 2, buttonSize, buttonSize, 0xcccccc, alpha).setInteractive();
+    const downButton = this.add.rectangle(dpadX, dpadY + buttonSize / 2, buttonSize, buttonSize, 0xcccccc, alpha).setInteractive();
+    const leftButton = this.add.rectangle(dpadX - buttonSize / 2, dpadY, buttonSize, buttonSize, 0xcccccc, alpha).setInteractive();
+    const rightButton = this.add.rectangle(dpadX + buttonSize / 2, dpadY, buttonSize, buttonSize, 0xcccccc, alpha).setInteractive();
+    
+    upButton.setScrollFactor(0);
+    downButton.setScrollFactor(0);
+    leftButton.setScrollFactor(0);
+    rightButton.setScrollFactor(0);
+
+    // --- Add Pointer Event Listeners ---
+    upButton.on('pointerdown', () => dpadState.up = true);
+    downButton.on('pointerdown', () => dpadState.down = true);
+    leftButton.on('pointerdown', () => dpadState.left = true);
+    rightButton.on('pointerdown', () => dpadState.right = true);
+
+    // ✨ THE FIX: Add 'pointerout' listeners to each button ✨
+    upButton.on('pointerout', () => dpadState.up = false);
+    downButton.on('pointerout', () => dpadState.down = false);
+    leftButton.on('pointerout', () => dpadState.left = false);
+    rightButton.on('pointerout', () => dpadState.right = false);
+
+    this.input.on('pointerup', () => {
+        dpadState.up = false;
+        dpadState.down = false;
+        dpadState.left = false;
+        dpadState.right = false;
+    });
 }
